21422b33dec836193fe79a76d5e3601dd1a19b36
Michael Persson <michael.ake.persson@gmail.com>
2018-09-24 17:04:37 -0500

    change to using a variadic method constructor

--- compress.go
+++ compress.go
@@ -5,32 +5,93 @@ import (
 	"io"
 )
 
-var algorithms = make(map[string]Algorithm)
+var methods = make(map[string]Method)
 
-// Algorithm interface.
-type Algorithm interface {
-	NewEncoder(w io.Writer, opts ...EncoderOption) (Encoder, error)
-	NewDecoder(r io.Reader, opts ...DecoderOption) (Decoder, error)
+// Method interface.
+type Method interface {
+	NewEncoder(w io.Writer) Encoder
+	NewDecoder(r io.Reader) Decoder
+	SetLevel(level Level) error
+	SetLitWidth(width int) error
+	SetEndian(endian Endian) error
 }
 
-// Register algorithm.
-func Register(name string, algorithm Algorithm) {
-	algorithms[name] = algorithm
+// Option variadic function.
+type Option func(Method) error
+
+// Level compression level.
+type Level int
+
+const (
+	// NoCompression no compression.
+	NoCompression Level = 0
+
+	// BestSpeed best speed.
+	BestSpeed Level = 1
+
+	// BestCompression best compression.
+	BestCompression Level = 9
+
+	// DefaultCompression default compression.
+	DefaultCompression Level = -1
+
+	// HuffmanOnly huffman only.
+	HuffmanOnly Level = -2
+)
+
+// Endian the order in which bytes are arranged into larger values.
+type Endian int
+
+const (
+	// Little endian LSB (Least Significant Bit).
+	Little Endian = 0
+
+	// Big endian MSB (Most Significant Bit).
+	Big Endian = 1
+)
+
+// Register method.
+func Register(name string, method Method) {
+	methods[name] = method
 }
 
-// Registered algorithm.
-func Registered(name string) (Algorithm, error) {
-	a, ok := algorithms[name]
+// NewMethod constructor.
+func NewMethod(name string, opts ...Option) (Method, error) {
+	m, ok := methods[name]
 	if !ok {
-		return nil, fmt.Errorf("algorithm not registered: %s", name)
+		return nil, fmt.Errorf("method not registered: %s", name)
 	}
-	return a, nil
+	return m.NewMethod(opts)
 }
 
-// Algorithms registered.
-func Algorithms() []string {
+// WithLevel compression level.
+// Supported by gzip, zlib.
+func WithLevel(level Level) Option {
+	return func(m Method) error {
+		return m.SetLevel(level)
+	}
+}
+
+// WithLitWidth the number of bit's to use for literal codes.
+// Supported by lzw.
+func WithLitWidth(width int) Option {
+	return func(m Method) error {
+		return m.SetLitWidth(width)
+	}
+}
+
+// WithEndian either MSB (most significant byte) or LSB (least significant byte).
+// Supported by lzw.
+func WithEndian(endian Endian) Option {
+	return func(m Method) error {
+		return m.SetEndian(endian)
+	}
+}
+
+// Methods registered.
+func Methods() []string {
 	l := []string{}
-	for a := range algorithms {
+	for a := range methods {
 		l = append(l, a)
 	}
 	return l
--- decoder.go
+++ decoder.go
@@ -9,45 +9,11 @@ import (
 type Decoder interface {
 	Read(v []byte) (int, error)
 	Close() error
-	SetOrder(o int) error // Should be SetEndian
-	SetLitWidth(w int) error
 }
-
-// DecoderOption function.
-type DecoderOption func(Decoder) error
-
-// NewDecoder variadic constructor.
-func NewDecoder(algo string, r io.Reader, opts ...DecoderOption) (Decoder, error) {
-	a, err := Registered(algo)
-	if err != nil {
-		return nil, err
-	}
-	return a.NewDecoder(r, opts...)
-}
-
-// WithLitWidth the number of bit's to use for literal codes.
-// Supported by lzw.
-/*
-func WithLitWidth(width int) DecoderOption {
-	return func(d Decoder) error {
-		return d.SetLitWidth(width)
-	}
-}
-*/
-
-// WithEndian either MSB (most significant byte) or LSB (least significant byte).
-// Supported by lzw.
-/*
-func WithEndian(endian Endian) DecoderOption {
-	return func(d Decoder) error {
-		return d.SetEndian(endian)
-	}
-}
-*/
 
 // Decode method.
-func Decode(name string, encoded []byte, opts ...DecoderOption) ([]byte, error) {
-	d, err := NewDecoder(name, bytes.NewBuffer(encoded), opts...)
+func (m *Method) Decode(encoded []byte) ([]byte, error) {
+	d, err := NewDecoder(bytes.NewBuffer(encoded))
 	if err != nil {
 		return nil, err
 	}
